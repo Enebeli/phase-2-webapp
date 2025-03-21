408045ed6b9122253efa83a4d9ea80dfd40cba68
ayabarrera <alyannabarrera@gmail.com>
2025-03-21 16:46:36 -0600

    added a reusable button component and css styling for this
    We have primary, secondary, and danger buttons

---
+++ app/components/Button.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./button.css";
+
+const Button = ({ children, onClick, type = "button", variant = "primary", disabled = false }) => {
+  return (
+    <button 
+      className={`custom-button ${variant}`} 
+      onClick={onClick} 
+      type={type} 
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired, 
+  onClick: PropTypes.func,
+  type: PropTypes.string, 
+  variant: PropTypes.oneOf(["primary", "secondary", "danger"]),
+  disabled: PropTypes.bool
+};
+
+export default Button;
