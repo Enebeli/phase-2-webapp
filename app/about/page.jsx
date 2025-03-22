dbf0eea7ac37b94f36cc57465d6096cb395dc0b9
ayabarrera <alyannabarrera@gmail.com>
2025-03-21 18:01:43 -0600

    added a page.jsx for about us

---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+    return (
+      <div className="about-container">
+        <h1>Our Story</h1>
+        <p>
+            LOREM IPSUM
+         </p>
+      </div>
+    );
+  }
+  
+  export default About;
