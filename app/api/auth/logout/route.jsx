8730d72735c04bb8d2e4f09d218331a46c5061d2
ayabarrera <alyannabarrera@gmail.com>
2025-03-24 15:46:19 -0600

    logout button is not working on vercel, try to refresh codes

--- app/api/auth/logout/route.jsx
+++ app/api/auth/logout/route.jsx
@@ -1,11 +1,16 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const url = request.nextUrl.clone();
-        url.pathname = "/login"
-  const response =  NextResponse.redirect(url);
+  url.pathname = "/login";
 
-  response.cookies.set("directus_session_token", "", {});
+  const response = NextResponse.redirect(url);
+
+  response.cookies.set("directus_session_token", "", {
+    path: "/", 
+    httpOnly: true, 
+    expires: new Date(0)
+  });
 
   return response;
 }
